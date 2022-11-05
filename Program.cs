6f088430831489f90e8032b5432cba0f8c3680b8
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-11-04 22:38:15 -0300

    [04/11/2022] Jogo do PIM.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿    Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Jogo do 1 2 3 PIM.");
+int i = 0;
+
+while (i < 40)
+{
+    i++;
+if (i%4 != 0)
+{
+        Console.ForegroundColor = ConsoleColor.White;
+Console.Write($"{i}, ");
+}
+else if (i < 40)
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+Console.Write($"PIM,\n");
+}
+else if (i == 40)
+{
+        Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write($"PIM.\n");
+}
+}
+Console.ResetColor();
